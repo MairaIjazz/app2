38fa22265198088780271ea801c7027ff448eee8
MairaIjazz <mairaiijaz@gmail.com>
2021-10-15 16:27:25 +0500

    commit

--- app.js
+++ app.js
@@ -62,48 +62,27 @@ dropbutton.addEventListener("click", function(){
  
  })
 
+//toggle button:
+var backdrop = document.querySelector('.backdrop');
+var togglebutton =document.querySelector('.toggle-button');
+var mobNav =document.querySelector('.mobile-nav');
 
-// slider:
 
-$num = $('.my-card').length;
-$even = $num / 2;
-$odd = ($num + 1) / 2;
 
-if ($num % 2 == 0) {
-  $('.my-card:nth-child(' + $even + ')').addClass('active');
-  $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
-  $('.my-card:nth-child(' + $even + ')').next().addClass('next');
-} else {
-  $('.my-card:nth-child(' + $odd + ')').addClass('active');
-  $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
-  $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
-}
 
-$('.my-card').click(function() {
-  $slide = $('.active').width();
-  console.log($('.active').position().left);
+// for closing the button:
+mobNav.addEventListener('click', function(){
+    // mobNav.style.display='none';
+    mobNav.classList.remove('open')
   
-  if ($(this).hasClass('next')) {
-    $('.card-carousel').stop(false, true).animate({left: '-=' + $slide});
-  } else if ($(this).hasClass('prev')) {
-    $('.card-carousel').stop(false, true).animate({left: '+=' + $slide});
-  }
-  
-  $(this).removeClass('prev next');
-  $(this).siblings().removeClass('prev active next');
-  
-  $(this).addClass('active');
-  $(this).prev().addClass('prev');
-  $(this).next().addClass('next');
 });
 
 
-// Keyboard nav
-$('html body').keydown(function(e) {
-  if (e.keyCode == 37) { // left
-    $('.active').prev().trigger('click');
-  }
-  else if (e.keyCode == 39) { // right
-    $('.active').next().trigger('click');
-  }
-});
+
+togglebutton.addEventListener('click', function(){
+  mobNav.classList.add('open');
+  setTimeout(function(){
+      backdrop.classList.add('open');
+  },10)
+  
+})
